3bcc7d8a226dcf660a6b103c01e60ff14df1dee3
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-05-29 17:10:42 +0200

    Switch to node package
    Setup environment for:
    - Typescript
    - express with ejs as the view engine

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import actuator from "express-actuator";
+import winston from "winston";
+import { errorLogger, requestLogger } from "./middleware";
+
+// Init logger
+const loggerInstance = winston.createLogger({
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: `data/ryujinx-ldn-website.log`,
+    }),
+  ],
+});
+
+export const logger = loggerInstance.child({
+  source: "Node",
+});
+
+// Init express server
+export const app = express();
+app.set('view engine', 'ejs');
+
+// This is set by NODE_ENV
+if (app.get("env") === "production") {
+  // Trust first proxy
+  app.set("trust proxy", 1);
+}
+
+// Readiness/Liveness probes and other application info
+app.use(actuator());
+
+// Logger middleware
+app.use(requestLogger);
+
+// Set up routes
+app.use(express.static("public"));
+
+// Error-handling
+app.use(errorLogger);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { env } from "process";
+import { app, logger } from "./app";
+import http from "http";
+
+const server = http.createServer(app);
+server.listen(parseInt(env.PORT || "3000"), (env.HOST || "127.0.0.1"));
+
+server.on("error", (error: Error) => {
+  logger.error("An error occurred.", error);
+});
+
+server.on("listening", () => {
+  logger.info("Startup done!", server.address());
+});
